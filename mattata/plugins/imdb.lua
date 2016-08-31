549c6fd92c738cc8e5c6b5f3a3357d307b2bcdaa
Matthew Hesketh <matthew@matthewhesketh.com>
2016-08-31 19:19:54 +0100

    Minor modifications

--- mattata/plugins/imdb.lua
+++ mattata/plugins/imdb.lua
@@ -1,51 +1,38 @@
 local imdb = {}
-
 local HTTP = require('socket.http')
 local URL = require('socket.url')
 local JSON = require('dkjson')
-local utilities = require('mattata.utilities')
-
-imdb.command = 'imdb <query>'
-
-function imdb:init(config)
-	imdb.triggers = utilities.triggers(self.info.username, config.cmd_pat):t('imdb', true).table
-	imdb.doc = config.cmd_pat .. 'imdb <query> \nReturns an IMDb entry.'
+local functions = require('mattata.functions')
+function imdb:init(configuration)
+	imdb.command = 'imdb <query>'
+	imdb.triggers = functions.triggers(self.info.username, configuration.command_prefix):t('imdb', true).table
+	imdb.doc = configuration.command_prefix .. 'imdb <query> \nReturns an IMDb entry.'
 end
-
-function imdb:action(msg, config)
-
-	local input = utilities.input(msg.text)
+function imdb:action(msg, configuration)
+	local input = functions.input(msg.text)
 	if not input then
 		if msg.reply_to_message and msg.reply_to_message.text then
 			input = msg.reply_to_message.text
 		else
-			utilities.send_message(self, msg.chat.id, imdb.doc, true, msg.message_id, true)
+			functions.send_message(self, msg.chat.id, imdb.doc, true, msg.message_id, true)
 			return
 		end
 	end
-
-	local url = 'http://www.omdbapi.com/?t=' .. URL.escape(input)
-
-	local jstr, res = HTTP.request(url)
+	local api = configuration.imdb_api .. URL.escape(input)
+	local raw_imdb_result, res = HTTP.request(api)
 	if res ~= 200 then
-		utilities.send_reply(self, msg, config.errors.connection)
+		functions.send_reply(self, msg, configuration.errors.connection)
 		return
 	end
-
-	local jdat = JSON.decode(jstr)
-
-	if jdat.Response ~= 'True' then
-		utilities.send_reply(self, msg, config.errors.results)
+	local decoded_imdb_result = JSON.decode(raw_imdb_result)
+	if decoded_imdb_result.Response ~= 'True' then
+		functions.send_reply(self, msg, configuration.errors.results)
 		return
 	end
-
-	local output = '*' .. jdat.Title .. ' ('.. jdat.Year ..')*\n'
-	output = output .. jdat.imdbRating ..'/10 | '.. jdat.Runtime ..' | '.. jdat.Genre ..'\n'
-	output = output .. '_' .. jdat.Plot .. '_\n'
-	output = output .. '[Read more.](http://imdb.com/title/' .. jdat.imdbID .. ')'
-
-	utilities.send_message(self, msg.chat.id, output, true, nil, true)
-
+	local output = '*' .. decoded_imdb_result.Title .. ' ('.. decoded_imdb_result.Year ..')*\n'
+	output = output .. decoded_imdb_result.imdbRating ..'/10 | '.. decoded_imdb_result.Runtime ..' | '.. decoded_imdb_result.Genre ..'\n'
+	output = output .. '_' .. decoded_imdb_result.Plot .. '_\n'
+	output = output .. '[Read more.](http://imdb.com/title/' .. decoded_imdb_result.imdbID .. ')'
+	functions.send_message(self, msg.chat.id, output, true, nil, true)
 end
-
 return imdb
