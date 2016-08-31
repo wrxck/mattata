f93e5120de36bf98480d8689350ec78b5d064af9
Matthew Hesketh <matthew@matthewhesketh.com>
2016-08-31 18:40:15 +0100

    Minor modifications

--- mattata/plugins/dice.lua
+++ mattata/plugins/dice.lua
@@ -1,23 +1,17 @@
 local dice = {}
-
-local utilities = require('mattata.utilities')
-
-dice.command = 'dice <number of dice> <range of numbers>'
-
-function dice:init(config)
-	dice.triggers = utilities.triggers(self.info.username, config.cmd_pat):t('dice', true).table
-	dice.doc = config.cmd_pat .. [[dice <number of dice to roll> <range of numbers on the dice>
+local functions = require('mattata.functions')
+function dice:init(configuration)
+	dice.command = 'dice <number of dice> <range of numbers>'
+	dice.triggers = functions.triggers(self.info.username, configuration.command_prefix):t('dice', true).table
+	dice.doc = configuration.command_prefix .. [[dice <number of dice to roll> <range of numbers on the dice>
 Rolls a die a given amount of times, with a given range.]]
 end
-
 function dice:action(msg)
-
-	local input = utilities.input(msg.text_lower)
+	local input = functions.input(msg.text_lower)
 	if not input then
-		utilities.send_message(self, msg.chat.id, dice.doc, true, msg.message_id, true)
+		functions.send_message(self, msg.chat.id, dice.doc, true, msg.message_id, true)
 		return
 	end
-
 	local count, range
 	if input:match('^[%d]+ [%d]+$') then
 		count, range = input:match('([%d]+) ([%d]+)')
@@ -25,30 +19,24 @@ function dice:action(msg)
 		count = 1
 		range = input:match('^d?([%d]+)$')
 	else
-		utilities.send_message(self, msg.chat.id, dice.doc, true, msg.message_id, true)
+		functions.send_message(self, msg.chat.id, dice.doc, true, msg.message_id, true)
 		return
 	end
-
 	count = tonumber(count)
 	range = tonumber(range)
-
 	if range < 2 then
-		utilities.send_reply(self, msg, 'The minimum range is 2.')
+		functions.send_reply(self, msg, 'The minimum range is 2.')
 		return
 	end
 	if range > 1000 or count > 1000 then
-		utilities.send_reply(self, msg, 'The maximum range and count are 1000.')
+		functions.send_reply(self, msg, 'The maximum range and count are 1000.')
 		return
 	end
-
 	local output = '*' .. count .. ' rolls with a range of ' .. range .. '*\n`'
 	for _ = 1, count do
 		output = output .. math.random(range) .. '\t'
 	end
 	output = output .. '`'
-
-	utilities.send_message(self, msg.chat.id, output, true, msg.message_id, true)
-
+	functions.send_message(self, msg.chat.id, output, true, msg.message_id, true)
 end
-
 return dice
