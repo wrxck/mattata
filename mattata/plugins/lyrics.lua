00a5565ab42bb059ec320a02a8e95453a4016323
Matthew Hesketh <matthew@matthewhesketh.com>
2016-08-31 19:33:39 +0100

    Modifications to formatting & minor additions

--- mattata/plugins/lyrics.lua
+++ mattata/plugins/lyrics.lua
@@ -1,43 +1,38 @@
 -- created by wrxck; based on a plugin by brawl
-
 local lyrics = {}
-
-local utilities = require('mattata.utilities')
+local functions = require('mattata.functions')
 local URL = require('socket.url')
 local HTTP = require('socket.http')
 local JSON = require('dkjson')
-
-function lyrics:init(config)
-    lyrics.triggers = utilities.triggers(self.info.username, config.cmd_pat):t('lyrics', true):t('lyric', true):t('sing', true).table
-    lyrics.doc = 'Find the lyrics to a song.'
-    lyricsnmusic_key = config.lyricsnmusi _key
+function lyrics:init(configuration)
+	lyrics.command = configuration.command_prefix .. 'lyrics <song>'
+    lyrics.triggers = functions.triggers(self.info.username, configuration.command_prefix):t('lyrics', true).table
+    lyrics.doc = 'Find the lyrics to the specified song.'
 end
-
 function lyrics:get(text)
+	local lyricsnmusic_key = configuration.lyricsnmusic_key
     local s = URL.escape(text)
     local r = HTTP.request("http://api.lyricsnmusic.com/songs?api_key=".. lyricsnmusic_key.."&q=" .. s)
     response = JSON.decode(r)
     local reply = ""
     if #response > 0 then
         local result = response[1]
-        reply = result.title .. " - " .. result.artist.name .. "\n\n" .. result.snippet .. "\n[Click to read more...](" .. result.url .. ")"
+        reply = result.title .. " - " .. result.artist.name .. "\n\n" .. result.snippet .. "\n[Click to see more...](" .. result.url .. ")"
     else
-        print('Error connecting to lyricsnmusic.com.')
+        print('There was an error whilst connecting to lyricsnmusic.com.')
     end
     return reply
 end
-
-function lyrics:action(msg, config, matches)
-    local input = utilities.input(msg.text)
+function lyrics:action(msg, configuration, matches)
+    local input = functions.input(msg.text)
     if not input then
         if msg.reply_to_message and msg.reply_to_message.text then
         input = msg.reply_to_message.text
     else
-        utilities.send_message(self, msg.chat.id, lyrics.doc, true, msg.message_id, true)
+        functions.send_message(self, msg.chat.id, lyrics.doc, true, msg.message_id, true)
         return
     end
 end
-utilities.send_reply(self, msg, lyrics:get(input), true)
+functions.send_reply(self, msg, lyrics:get(input), true)
 end
-
 return lyrics
