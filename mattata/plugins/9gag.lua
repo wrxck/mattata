268e8b96877b268d6249038519e8632ab1fb795b
Matthew Hesketh <matthew@matthewhesketh.com>
2016-08-31 17:42:23 +0100

    Terminology edits, de-cluttering & formatting

--- mattata/plugins/9gag.lua
+++ mattata/plugins/9gag.lua
@@ -3,24 +3,24 @@
 
 local HTTP = require('socket.http')
 local JSON = require('dkjson')
-local utilities = require('mattata.utilities')
-local bindings = require('mattata.bindings')
+local functions = require('mattata.functions')
+local telegram_api = require('mattata.telegram_api')
 
 local ninegag = {}
 
-function ninegag:init(config)
-    ninegag.triggers = utilities.triggers(self.info.username, config.cmd_pat):t('9gag', true).table
+function ninegag:init(configuration)
+    ninegag.triggers = functions.triggers(self.info.username, configuration.command_prefix):t('9gag', true).table
     ninegag.command = '9gag'
-    ninegag.doc = config.cmd_pat .. [[9gag ...
+    ninegag.doc = configuration.command_prefix .. [[9gag ...
 Returns a random image from the latest 9gag posts.
     ]]
 end
 
-function ninegag:action(msg, config)
+function ninegag:action(msg, configuration)
     local url = "http://api-9gag.herokuapp.com/"
     local jstr, res = HTTP.request(url)
     if res ~= 200 then
-        utilities.send_reply(self, msg, config.errors.connection)
+        functions.send_reply(self, msg, configuration.errors.connection)
         return
     end
     local jstr = HTTP.request(url)
@@ -28,7 +28,7 @@ function ninegag:action(msg, config)
     local math = math.random(#jdat)
     local random = jdat[math].src
     local output = '[XD]('..random..')'
-    utilities.send_message(self, msg.chat.id, output, false, true)
+    functions.send_message(self, msg.chat.id, output, false, true)
 end
 
 return ninegag
