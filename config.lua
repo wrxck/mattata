f59b6d372f3b59064d3a25e5d744e68396b5827e
Matthew Hesketh <matthew@matthewhesketh.com>
2016-08-31 17:19:04 +0100

    Extensive changes & minor edits

--- config.lua
+++ config.lua
@@ -1,42 +1,31 @@
 return {
-    bot_api_key = '', -- insert your bot API token you received from BotFather
-    admin = , -- the numerical ID of the owner, presumably you
-    lang = 'en', -- two digit language code
-    log_chat = , -- the numerical ID of the chat you wish to log any errors to
-    cli_port = 4569, -- the port to use for CLI 
+    bot_api_key = '', -- insert the bot API token you received from BotFather
+    admin = , -- the numerical ID of the owner, who is presumably you
+    lang = 'en', -- two digit locale
+    log_chat = , -- the numerical ID of the chat you wish to log errors/private messages to, you can add telegram.me/groupinfobot to your group to view this information, if necessary
     about_text = [[
 I am mattata, a multi-purpose Telegram bot.
 
 Send /help to get started.
     ]],
-    cmd_pat = '/',
-    drua_block_on_blacklist = false,
-    bing_api_key = '',
-    google_api_key = '',
-    google_cse_key = '',
-    owm_api_key = '',
-    lastfm_api_key = '',
-    biblia_api_key = '',
-    thecatapi_key = '',
-    nasa_api_key = '',
-    yandex_key = '',
-    lyricsnmusic_key = '',
+    command_prefix = '/',
+    thecatapi_key = '', -- you can get one of these by heading to http://thecatapi.com/api-key-registration.html
+    yandex_key = '', -- you can get one of these by heading to https://tech.yandex.com/keys/get/?service=trnsl
+    lyricsnmusic_key = '', -- you can get one of these by heading to http://www.lyricsnmusic.com/api_keys/new
     errors = {
-        generic = 'Error.',
-        connection = 'Connection error.',
-        results = 'No results found.',
-        argument = 'Invalid argument.',
-        syntax = 'Invalid syntax.',
+        generic = 'WELP. That\'s an error!',
+        connection = 'I\'m sorry, but there was a connection error whilst processing your request, please try again later.',
+        results = 'I\'m sorry, but I couldn\'t find any results for that.',
+        argument = 'I\'m sorry, but the given arguments were either invalid or non-existent.',
+        syntax = 'Error. Invalid syntax.',
     },
-    mattata = {
-        cleverbot_api = 'https://brawlbot.tk/apis/chatter-bot-api/cleverbot.php?text=',
-        connection = 'I don\'t feel like talking right now.',
-        response = 'I don\'t know what to say to that.'
+    messaging = {
+        api_url = 'https://brawlbot.tk/apis/chatter-bot-api/cleverbot.php?text=',
+        connection_error = 'Rowan\'s words echoed: There\'s a time and place for everything! But not now.',
+        response_error = 'I\'m not sure how to answer that...'
     },
     plugins = {
         'control',
-        'administration',
-        'blacklist',
         'about',
         'id',
         'nick',
@@ -76,13 +65,13 @@ Send /help to get started.
         'skateipsum',
         'starwars',
         'lua',
-        'cleverbot',
         'setandget',
         'reactions',
         '9gag',
         'lyrics',
         'fact',
         'help',
-        'autoresponses'
+        'autoresponses',
+        'messaging'
     }
 }
