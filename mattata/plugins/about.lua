b21dd1961267ae3941438e694706084c02b33118
Matthew Hesketh <matthew@matthewhesketh.com>
2016-08-31 17:44:23 +0100

    Terminology edits, de-cluttering & formatting

--- mattata/plugins/about.lua
+++ mattata/plugins/about.lua
@@ -14,10 +14,10 @@ function about:action(msg, configuration)
 	local output = configuration.about_text .. '\nCreated by wrxck. Based on the otouto project by topkecleon.'
 	if
 		(msg.new_chat_member and msg.new_chat_member.id == self.info.id)
-		or msg.text_lower:match('^'..config.command_prefix..'about$')
-		or msg.text_lower:match('^'..config.command_prefix..'about@'..self.info.username:lower()..'$')
-		or msg.text_lower:match('^'..config.command_prefix..'start$')
-		or msg.text_lower:match('^'..config.command_prefix..'start@'..self.info.username:lower()..'$')
+		or msg.text_lower:match('^'..configuration.command_prefix..'about$')
+		or msg.text_lower:match('^'..configuration.command_prefix..'about@'..self.info.username:lower()..'$')
+		or msg.text_lower:match('^'..configuration.command_prefix..'start$')
+		or msg.text_lower:match('^'..configuration.command_prefix..'start@'..self.info.username:lower()..'$')
 	then
 		functions.send_message(self, msg.chat.id, output, true, nil, true)
 		return
