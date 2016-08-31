c2bcb9b062cd33734640e326be3ed95df95661d6
Matthew Hesketh <matthew@matthewhesketh.com>
2016-08-31 17:43:53 +0100

    Terminology edits, de-cluttering & formatting

--- mattata/plugins/about.lua
+++ mattata/plugins/about.lua
@@ -1,34 +1,28 @@
 local about = {}
 
 local bot = require('mattata.bot')
-local utilities = require('mattata.utilities')
+local functions = require('mattata.functions')
 
 about.command = 'about'
 about.doc = 'Information about mattata.'
-
 about.triggers = {
 	''
 }
 
-function about:action(msg, config)
-
+function about:action(msg, configuration)
 	if msg.forward_from then return end
-
-	local output = config.about_text .. '\nCreated by wrxck. Based on the otouto project by topkecleon.'
-
+	local output = configuration.about_text .. '\nCreated by wrxck. Based on the otouto project by topkecleon.'
 	if
 		(msg.new_chat_member and msg.new_chat_member.id == self.info.id)
-		or msg.text_lower:match('^'..config.cmd_pat..'about$')
-		or msg.text_lower:match('^'..config.cmd_pat..'about@'..self.info.username:lower()..'$')
-		or msg.text_lower:match('^'..config.cmd_pat..'start$')
-		or msg.text_lower:match('^'..config.cmd_pat..'start@'..self.info.username:lower()..'$')
+		or msg.text_lower:match('^'..config.command_prefix..'about$')
+		or msg.text_lower:match('^'..config.command_prefix..'about@'..self.info.username:lower()..'$')
+		or msg.text_lower:match('^'..config.command_prefix..'start$')
+		or msg.text_lower:match('^'..config.command_prefix..'start@'..self.info.username:lower()..'$')
 	then
-		utilities.send_message(self, msg.chat.id, output, true, nil, true)
+		functions.send_message(self, msg.chat.id, output, true, nil, true)
 		return
 	end
-
 	return true
-
 end
 
 return about
