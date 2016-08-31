8a476950a39fe0d09aec6491d3a19e41988667cd
Matthew Hesketh <matthew@matthewhesketh.com>
2016-08-31 19:44:42 +0100

    Modifications to formatting & minor additions

--- mattata/plugins/nick.lua
+++ mattata/plugins/nick.lua
@@ -1,40 +1,32 @@
 local nick = {}
-
-local utilities = require('mattata.utilities')
-
-nick.command = 'nick <nickname>'
-
-function nick:init(config)
-	nick.triggers = utilities.triggers(self.info.username, config.cmd_pat):t('nick', true).table
-	nick.doc = config.cmd_pat .. [[nick <nickname>
-Set your nickname. Use "]] .. config.cmd_pat .. 'nick --" to delete it.'
+local functions = require('mattata.functions')
+function nick:init(configuration)
+	nick.command = 'nick <nickname>'
+	nick.triggers = functions.triggers(self.info.username, configuration.command_prefix):t('nick', true).table
+	nick.doc = configuration.command_prefix .. [[nick <nickname>
+Set your nickname. Use "]] .. configuration.command_prefix .. 'nick --" to delete it.'
 end
-
-function nick:action(msg, config)
-
+function nick:action(msg, configuration)
 	local id_str, name
-
-	if msg.from.id == config.admin and msg.reply_to_message then
+	if msg.from.id == configuration.admin and msg.reply_to_message then
 		id_str = tostring(msg.reply_to_message.from.id)
-		name = utilities.build_name(msg.reply_to_message.from.first_name, msg.reply_to_message.from.last_name)
+		name = functions.build_name(msg.reply_to_message.from.first_name, msg.reply_to_message.from.last_name)
 	else
 		id_str = tostring(msg.from.id)
-		name = utilities.build_name(msg.from.first_name, msg.from.last_name)
+		name = functions.build_name(msg.from.first_name, msg.from.last_name)
 	end
-
 	self.database.userdata[id_str] = self.database.userdata[id_str] or {}
-
 	local output
-	local input = utilities.input(msg.text)
+	local input = functions.input(msg.text)
 	if not input then
 		if self.database.userdata[id_str].nickname then
 			output = name .. '\'s nickname is "' .. self.database.userdata[id_str].nickname .. '".'
 		else
 			output = name .. ' currently has no nickname.'
 		end
-	elseif utilities.utf8_len(input) > 32 then
-		output = 'The character limit for nicknames is 32.'
-	elseif input == '--' or input == utilities.char.em_dash then
+	elseif functions.utf8_len(input) > 25 then
+		output = 'The character limit for nicknames is 25.'
+	elseif input == '--' or input == functions.char.em_dash then
 		self.database.userdata[id_str].nickname = nil
 		output = name .. '\'s nickname has been deleted.'
 	else
@@ -42,9 +34,6 @@ function nick:action(msg, config)
 		self.database.userdata[id_str].nickname = input
 		output = name .. '\'s nickname has been set to "' .. input .. '".'
 	end
-
-	utilities.send_reply(self, msg, output)
-
+	functions.send_reply(self, msg, output)
 end
-
 return nick
