88bb9adbfc6c700dfa507d0b6f6ddd5748b02bd7
Matthew Hesketh <matthew@matthewhesketh.com>
2016-08-31 18:57:16 +0100

    Minor edits & formatting adjustments

--- mattata/plugins/help.lua
+++ mattata/plugins/help.lua
@@ -1,48 +1,42 @@
 local help = {}
-
-local utilities = require('mattata.utilities')
-
+local functions = require('mattata.functions')
 local help_text
-
-function help:init(config)
-	local commandlist = {}
-	help_text = '*Commands I understand include:*\n• '..config.cmd_pat
+function help:init(configuration)
+	local command_list = {}
+	help_text = '*Commands I understand include:*\n» '..configuration.command_prefix
 	for _,plugin in ipairs(self.plugins) do
 		if plugin.command then
-			table.insert(commandlist, plugin.command)
+			table.insert(command_list, plugin.command)
 			if plugin.doc then
-				plugin.help_word = utilities.get_word(plugin.command, 1)
+				plugin.help_word = functions.get_word(plugin.command, 1)
 			end
 		end
 	end
-	table.insert(commandlist, 'help [command]')
-	table.sort(commandlist)
-	help_text = help_text .. table.concat(commandlist, '\n• '..config.cmd_pat) .. '\nArguments: <required> [optional]'
+	table.insert(command_list, 'help [command]')
+	table.sort(command_list)
+	help_text = help_text .. table.concat(command_list, '\n» '..configuration.command_prefix) .. '\nArguments: <required> [optional]'
 	help_text = help_text:gsub('%[', '\\[')
-	help.triggers = utilities.triggers(self.info.username, config.cmd_pat):t('help', true):t('h', true).table
-	help.doc = config.cmd_pat .. '*help [command]* \n*Usage information for the given command.*'
+	help.triggers = functions.triggers(self.info.username, configuration.command_prefix):t('help', true):t('h', true).table
+	help.doc = configuration.command_prefix .. '*help [command]* \n*Usage information for the given command.*'
 end
-
 function help:action(msg)
-	local input = utilities.input(msg.text_lower)
+	local input = functions.input(msg.text_lower)
 	if input then
 		for _,plugin in ipairs(self.plugins) do
 			if plugin.help_word == input:gsub('^/', '') then
 				local output = '*Help for* _' .. plugin.help_word .. '_ *:*\n' .. plugin.doc
-				utilities.send_message(self, msg.chat.id, output, true, nil, true)
+				functions.send_message(self, msg.chat.id, output, true, nil, true)
 				return
 			end
 		end
-		utilities.send_reply(self, msg, '*Sorry, there is no help for that command!*')
+		functions.send_message(self, msg, '*Sorry, there is no help for that command!*', true, nil, true)
 	else
-
-		local res = utilities.send_message(self, msg.from.id, help_text, true, nil, true)
+		local res = functions.send_message(self, msg.from.id, help_text, true, nil, true)
 		if not res then
-			utilities.send_reply(self, msg, 'Please [message me in a private chat](http://telegram.me/' .. self.info.username .. '?start=help) for command-related help.', true)
+			functions.send_message(self, msg, 'Please [message me in a private chat](http://telegram.me/' .. self.info.username .. '?start=help) for command-related help.', true, nil, true)
 		elseif msg.chat.type ~= 'private' then
-			utilities.send_reply(self, msg, '*Hi there, mate! I have sent you a private message!*')
+			functions.send_message(self, msg, '*Hi there, mate! I have sent you a private message!*', true, nil, true)
 		end
 	end
 end
-
 return help
