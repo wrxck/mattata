6cdedd8a9365e9a3288cde0da01142f528a0b2de
Matthew Hesketh <matthew@matthewhesketh.com>
2016-08-31 17:56:51 +0100

    Terminology updates and configuration additions

--- mattata/plugins/baconipsum.lua
+++ mattata/plugins/baconipsum.lua
@@ -1,14 +1,14 @@
 local baconipsum = {}
-local utilities = require('mattata.utilities')
+local functions = require('mattata.functions')
 local HTTPS = require('ssl.https')
-function baconipsum:init(config)
-	baconipsum.triggers = utilities.triggers(self.info.username, config.cmd_pat):t('baconipsum', true).table
-baconipsum.command = 'baconipsum'
-baconipsum.doc = 'Generates a few meaty Lorem Ipsum sentences!'
+function baconipsum:init(configuration)
+	baconipsum.triggers = functions.triggers(self.info.username, configuration.command_prefix):t('baconipsum', true).table
+	baconipsum.command = 'baconipsum'
+	baconipsum.doc = 'Generate a few meaty Lorem Ipsum sentences!'
 end
-function baconipsum:action(msg, config)
- local url = 'https://baconipsum.com/api/?type=all-meat&sentences=3&start-with-lorem=1&format=text'
- local output = HTTPS.request(url)
-	utilities.send_message(self, msg.chat.id, output, nil, true)
+function baconipsum:action(msg, configuration)
+    local api = configuration.baconipsum_api
+    local output = HTTPS.request(api)
+    functions.send_message(self, msg.chat.id, output, nil, true)
 end
 return baconipsum
