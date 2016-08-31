65ba7e661978297c804d13356450b690c474fe58
Matthew Hesketh <matthew@matthewhesketh.com>
2016-08-31 18:02:44 +0100

    Minor modifications

--- mattata/plugins/calc.lua
+++ mattata/plugins/calc.lua
@@ -1,41 +1,30 @@
 local calc = {}
-
 local URL = require('socket.url')
 local HTTPS = require('ssl.https')
-local utilities = require('mattata.utilities')
-
+local functions = require('mattata.functions')
 calc.command = 'calc <expression>'
-
-function calc:init(config)
-	calc.triggers = utilities.triggers(self.info.username, config.cmd_pat):t('calc', true).table
-	calc.doc = config.cmd_pat .. [[calc <expression>
-Calculates solutions to mathematical expressions and conversions between common units. The results are provided by mathjs.org.]]
+function calc:init(configuration)
+	calc.triggers = functions.triggers(self.info.username, configuration.command_prefix):t('calc', true).table
+	calc.doc = configuration.command_prefix .. [[calc <expression>
+Calculates solutions to, well, mathematical expressions. The results are provided by mathjs.org.]]
 end
-
-function calc:action(msg, config)
-
-	local input = utilities.input(msg.text)
+function calc:action(msg, configuration)
+	local input = functions.input(msg.text)
 	if not input then
 		if msg.reply_to_message and msg.reply_to_message.text then
 			input = msg.reply_to_message.text
 		else
-			utilities.send_message(self, msg.chat.id, calc.doc, true, msg.message_id, true)
+			functions.send_message(self, msg.chat.id, calc.doc, true, msg.message_id, true)
 			return
 		end
 	end
-
-	local url = 'https://api.mathjs.org/v1/?expr=' .. URL.escape(input)
-
+	local url = configuration.calc_api .. URL.escape(input)
 	local output = HTTPS.request(url)
 	if not output then
-		utilities.send_reply(self, msg, config.errors.connection)
+		functions.send_reply(self, msg, configuration.errors.connection_error)
 		return
 	end
-
 	output = '`' .. output .. '`'
-
-	utilities.send_message(self, msg.chat.id, output, true, msg.message_id, true)
-
+	functions.send_message(self, msg.chat.id, output, true, msg.message_id, true)
 end
-
 return calc
