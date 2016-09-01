3b71d11024ec9ed628280de0395615c40f00ade1
Matthew Hesketh <matthew@matthewhesketh.com>
2016-09-01 21:50:04 +0100

    Minor changes

--- mattata/plugins/time.lua
+++ mattata/plugins/time.lua
@@ -1,60 +1,46 @@
 local time = {}
-
 local HTTPS = require('ssl.https')
 local JSON = require('dkjson')
-local utilities = require('mattata.utilities')
-
+local functions = require('mattata.functions')
 time.command = 'time <location>'
-
-function time:init(config)
-	time.triggers = utilities.triggers(self.info.username, config.cmd_pat):t('time', true).table
-	time.doc = config.cmd_pat .. [[time <location>
+function time:init(configuration)
+	time.triggers = functions.triggers(self.info.username, configuration.command_prefix):t('time', true).table
+	time.doc = configuration.command_prefix .. [[time <location>
 Returns the time, date, and timezone for the given location.]]
 end
-
-function time:action(msg, config)
-
-	local input = utilities.input(msg.text)
+function time:action(msg, configuration)
+	local input = functions.input(msg.text)
 	if not input then
 		if msg.reply_to_message and msg.reply_to_message.text then
 			input = msg.reply_to_message.text
 		else
-			utilities.send_message(self, msg.chat.id, time.doc, true, msg.message_id, true)
+			functions.send_message(self, msg.chat.id, time.doc, true, msg.message_id, true)
 			return
 		end
 	end
-
-	local coords = utilities.get_coords(input, config)
+	local coords = functions.get_coords(input, configuration)
 	if type(coords) == 'string' then
-		utilities.send_reply(self, msg, coords)
+		functions.send_reply(self, msg, coords)
 		return
 	end
-
 	local now = os.time()
 	local utc = os.time(os.date("!*t", now))
-
 	local url = 'https://maps.googleapis.com/maps/api/timezone/json?location=' .. coords.lat ..','.. coords.lon .. '&timestamp='..utc
-
 	local jstr, res = HTTPS.request(url)
 	if res ~= 200 then
-		utilities.send_reply(self, msg, config.errors.connection)
+		functions.send_reply(self, msg, configuration.errors.connection)
 		return
 	end
-
 	local jdat = JSON.decode(jstr)
-
 	local timestamp = now + jdat.rawOffset + jdat.dstOffset
 	local utcoff = (jdat.rawOffset + jdat.dstOffset) / 3600
 	if utcoff == math.abs(utcoff) then
-		utcoff = '+'.. utilities.pretty_float(utcoff)
+		utcoff = '+'.. functions.pretty_float(utcoff)
 	else
-		utcoff = utilities.pretty_float(utcoff)
+		utcoff = functions.pretty_float(utcoff)
 	end
 	local output = os.date('!%I:%M %p\n', timestamp) .. os.date('!%A, %B %d, %Y\n', timestamp) .. jdat.timeZoneName .. ' (UTC' .. utcoff .. ')'
 	output = '```\n' .. output .. '\n```'
-
-	utilities.send_reply(self, msg, output, true)
-
+	functions.send_reply(self, msg, output, true)
 end
-
 return time
