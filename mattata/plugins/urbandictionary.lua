19da2a25b5f431d7cc7a2dfa2371d3d6ed0fa2a6
Matthew Hesketh <matthew@matthewhesketh.com>
2016-09-01 21:42:23 +0100

    Minor changes

--- mattata/plugins/urbandictionary.lua
+++ mattata/plugins/urbandictionary.lua
@@ -1,55 +1,42 @@
 local urbandictionary = {}
-
 local HTTP = require('socket.http')
 local URL = require('socket.url')
 local JSON = require('dkjson')
-local utilities = require('mattata.utilities')
-
+local functions = require('mattata.functions')
 urbandictionary.command = 'urbandictionary <query>'
-
-function urbandictionary:init(config)
-	urbandictionary.triggers = utilities.triggers(self.info.username, config.cmd_pat)
+function urbandictionary:init(configuration)
+	urbandictionary.triggers = functions.triggers(self.info.username, configuration.command_prefix)
 		:t('urbandictionary', true):t('ud', true):t('urban', true).table
-	urbandictionary.doc = config.cmd_pat .. [[urbandictionary <query>
-Returns a definition from Urban Dictionary.
-Aliases: ]] .. config.cmd_pat .. 'ud, ' .. config.cmd_pat .. 'urban'
+	urbandictionary.doc = configuration.command_prefix .. [[urbandictionary <query>
+Defines the given word - the Urban Dictionary way.
+Aliases: ]] .. configuration.command_prefix .. 'ud, ' .. configuration.command_prefix .. 'urban'
 end
-
-function urbandictionary:action(msg, config)
-
-	local input = utilities.input(msg.text)
+function urbandictionary:action(msg, configuration)
+	local input = functions.input(msg.text)
 	if not input then
 		if msg.reply_to_message and msg.reply_to_message.text then
 			input = msg.reply_to_message.text
 		else
-			utilities.send_message(self, msg.chat.id, urbandictionary.doc, true, msg.message_id, true)
+			functions.send_message(self, msg.chat.id, urbandictionary.doc, true, msg.message_id, true)
 			return
 		end
 	end
-
 	local url = 'http://api.urbandictionary.com/v0/define?term=' .. URL.escape(input)
-
 	local jstr, res = HTTP.request(url)
 	if res ~= 200 then
-		utilities.send_reply(self, msg, config.errors.connection)
+		functions.send_reply(self, msg, configuration.errors.connection)
 		return
 	end
-
 	local jdat = JSON.decode(jstr)
 	if jdat.result_type == "no_results" then
-		utilities.send_reply(self, msg, config.errors.results)
+		functions.send_reply(self, msg, configuration.errors.results)
 		return
 	end
-
-	local output = '*' .. jdat.list[1].word .. '*\n\n' .. utilities.trim(jdat.list[1].definition)
+	local output = '*' .. jdat.list[1].word .. '*\n\n' .. functions.trim(jdat.list[1].definition)
 	if string.len(jdat.list[1].example) > 0 then
-		output = output .. '_\n\n' .. utilities.trim(jdat.list[1].example) .. '_'
+		output = output .. '_\n\n' .. functions.trim(jdat.list[1].example) .. '_'
 	end
-
 	output = output:gsub('%[', ''):gsub('%]', '')
-
-	utilities.send_message(self, msg.chat.id, output, true, nil, true)
-
+	functions.send_message(self, msg.chat.id, output, true, nil, true)
 end
-
 return urbandictionary
