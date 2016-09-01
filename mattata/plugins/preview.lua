84a35415ee78c75b0fd51f3077de7e82543ee6e4
Matthew Hesketh <matthew@matthewhesketh.com>
2016-09-01 21:04:46 +0100

    Minor changes

--- mattata/plugins/preview.lua
+++ mattata/plugins/preview.lua
@@ -1,43 +1,31 @@
 local preview = {}
-
 local HTTP = require('socket.http')
-local utilities = require('mattata.utilities')
-
-preview.command = 'preview <link>'
-
-function preview:init(config)
-	preview.triggers = utilities.triggers(self.info.username, config.cmd_pat):t('preview', true).table
-	preview.doc = config.cmd_pat .. 'preview <link> \nReturns a full-message, "unlinked" preview.'
+local functions = require('mattata.functions')
+function preview:init(configuration)
+    preview.command = 'preview <url>'
+    preview.triggers = functions.triggers(self.info.username, configuration.command_prefix):t('preview', true).table
+    preview.doc = configuration.command_prefix .. 'preview <link> \nSends an "unlinked" preview of the given URL.'
 end
-
 function preview:action(msg)
-
-	local input = utilities.input(msg.text)
-
-	if not input then
-		utilities.send_message(self, msg.chat.id, preview.doc, true, nil, true)
-		return
-	end
-
-	input = utilities.get_word(input, 1)
-	if not input:match('^https?://.+') then
-		input = 'http://' .. input
-	end
-
-	local res = HTTP.request(input)
-	if not res then
-		utilities.send_reply(self, msg, 'Please provide a valid link.')
-		return
-	end
-
-	if res:len() == 0 then
-		utilities.send_reply(self, msg, 'Sorry, the link you provided is not letting me generate a preview.')
-		return
-	end
-
-	local output = '[​](' .. input .. ')'
-	utilities.send_message(self, msg.chat.id, output, false, nil, true)
-
+    local input = functions.input(msg.text)
+    if not input then
+        functions.send_message(self, msg.chat.id, preview.doc, true, nil, true)
+        return
+    end
+    input = functions.get_word(input, 1)
+    if not input:match('^https?://.+') then
+        input = 'http://' .. input
+    end
+    local res = HTTP.request(input)
+    if not res then
+        functions.send_reply(self, msg, 'Please provide a valid URL.')
+        return
+    end
+    if res:len() == 0 then
+        functions.send_reply(self, msg, 'Sorry, the URL you provided is not letting me generate a preview. Please check it\'s a valid link.')
+        return
+    end
+    local output = '[​](' .. input .. ')'
+    functions.send_message(self, msg.chat.id, output, false, nil, true)
 end
-
 return preview
