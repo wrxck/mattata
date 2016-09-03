c61420cefdd8dc16aa75f68cfaa5f58e1e950712
Matthew Hesketh <matthew@matthewhesketh.com>
2016-09-03 16:53:45 +0100

    Minor fixes

--- mattata/plugins/cats.lua
+++ mattata/plugins/cats.lua
@@ -2,20 +2,19 @@ local cats = {}
 local HTTP = require('socket.http')
 local functions = require('mattata.functions')
 function cats:init(configuration)
-	cats.triggers = functions.triggers(self.info.username, configuration.command_prefix):t('cat').table
-	cats.command = 'cat'
-	cats.doc = 'A random picture of a cat!'
+    cats.command = 'cat'
+    cats.triggers = functions.triggers(self.info.username, configuration.command_prefix):t('cat').table
+    cats.doc = 'A random picture of a cat!'
 end
 function cats:action(msg, configuration)
-	local api = configuration.cat_api .. '&api_key=' .. configuration.cat_api_key
-	end
-	local str, res = HTTP.request(api)
-	if res ~= 200 then
-		functions.send_reply(self, msg, configuration.errors.connection)
-		return
-	end
-	str = str:match('<img src="(.-)">')
-	local output = '[Meow!]('..str..')'
-	functions.send_message(self, msg.chat.id, output, false, nil, true)
+    local api = configuration.cat_api .. '&api_key=' .. configuration.cat_api_key
+    local str, res = HTTP.request(api)
+    if res ~= 200 then
+        functions.send_reply(self, msg, configuration.errors.connection)
+        return
+    end
+    str = str:match('<img src="(.-)">')
+    local output = '[Meow!]('..str..')'
+    functions.send_message(self, msg.chat.id, output, false, nil, true)
 end
 return cats
