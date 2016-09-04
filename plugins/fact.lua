1f866a1d21f84d7e4bc58e069a0fa73c7cae8c00
Matthew Hesketh <matthew@matthewhesketh.com>
2016-09-04 09:45:47 +0100

    Minor fixes

--- plugins/fact.lua
+++ plugins/fact.lua
@@ -13,12 +13,8 @@ function fact:action(msg, configuration)
  local api = configuration.fact_api
  local raw_fact = HTTP.request(api)
  local decoded_fact = JSON.decode(raw_fact)
- if decoded_fact.error then
-  functions.send_reply(self, msg, configuration.errors.results)
-  return
- end
- local math = math.random(#jdat)
+ local math = math.random(#decoded_fact)
  local output = decoded_fact[math].nid:gsub('<p>',''):gsub('</p>',''):gsub('&amp;','&'):gsub('<em>',''):gsub('</em>',''):gsub('<strong>',''):gsub('</strong>','')
- functions.send_message(self, msg.chat.id, output, nil, true)
+ functions.send_message(self, msg.chat.id, output, true, nil, true)
 end
 return fact
