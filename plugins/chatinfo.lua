c06d86e47fb9d745255922b4d91520cb03297685
Matthew Hesketh <matthew@matthewhesketh.com>
2016-09-26 10:42:15 +0100

    mattata v2.0.1
    Minor additions

--- plugins/chatinfo.lua
+++ plugins/chatinfo.lua
@@ -5,10 +5,14 @@ function chatinfo:init(configuration)
 	chatinfo.triggers = functions.triggers(self.info.username, configuration.command_prefix):t('chatinfo', true).table
 end
 function chatinfo:action(msg)
+	local output = ''
 	if msg.chat.type ~= 'private' then
-		functions.send_reply(msg, msg.chat.title .. ' (' .. msg.chat.type .. ' | ' .. msg.chat.id .. ')', true)
+		output = 'You are speaking in the ' .. msg.chat.type .. ' \'*' .. msg.chat.title .. '*\' ' .. '(`' .. msg.chat.id .. '`).'
+		functions.send_reply(msg, output, true)
 		return
 	else
+		output = 'Hello, *' .. functions.get_name(msg) .. '*. Your ID is `' .. msg.from.id .. '`.'
+		functions.send_reply(msg, output, true)
 		return
 	end
 end
