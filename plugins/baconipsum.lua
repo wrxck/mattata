cb9b782a75e255ec1676c777c2cb581c6ab5c5da
Matthew Hesketh <matthew@matthewhesketh.com>
2016-09-30 10:52:54 +0100

    mattata v2.1
    Made baconipsum.lua inline

--- plugins/baconipsum.lua
+++ plugins/baconipsum.lua
@@ -4,7 +4,14 @@ local HTTPS = require('ssl.https')
 function baconipsum:init(configuration)
 	baconipsum.command = 'baconipsum'
 	baconipsum.triggers = functions.triggers(self.info.username, configuration.command_prefix):t('baconipsum', true).table
+	baconipsum.inline_triggers = baconipsum.triggers
 	baconipsum.doc = configuration.command_prefix .. 'baconipsum - Generate a few meaty Lorem Ipsum sentences!'
+end
+function baconipsum:inline_callback(inline_query, configuration)
+    local str, res = HTTPS.request(configuration.baconipsum_api)
+    local output = '`' .. str .. '`'
+	local results = '[{"type":"article","id":"10","title":"/baconipsum","description":"Generate a few meaty Lorem Ipsum sentences!","input_message_content":{"message_text":"' .. output .. '","parse_mode":"Markdown"}}]'
+	functions.answer_inline_query(inline_query, results, 900)
 end
 function baconipsum:action(msg, configuration)
     local str, res = HTTPS.request(configuration.baconipsum_api)
