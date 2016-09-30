bdaa63aca76c1fa9129d7c42eecf89553ee203eb
Matthew Hesketh <matthew@matthewhesketh.com>
2016-09-30 10:49:22 +0100

    mattata v2.1
    Started making plugins inline. apod.lua, for example, is now inline.

--- plugins/about.lua
+++ plugins/about.lua
@@ -8,7 +8,7 @@ function about:action(msg, configuration)
 	if msg.forward_from then
 		return
 	end
-	local output = configuration.about_text .. '\nCreated by wrxck. Based on the otouto project by topkecleon.'
+	local output = configuration.about_text .. '\nCreated by wrxck. Originally based on otouto by topkecleon.'
 	if (msg.new_chat_member and msg.new_chat_member.id == self.info.id) or msg.text_lower:match('^' .. configuration.command_prefix .. 'about$') or msg.text_lower:match('^' .. configuration.command_prefix .. 'about@' .. self.info.username:lower() .. '$') or msg.text_lower:match('^' .. configuration.command_prefix .. 'start$') or msg.text_lower:match('^' .. configuration.command_prefix .. 'start@' .. self.info.username:lower() .. '$') then
 		functions.send_reply(msg, '`' .. output .. '`', true)
 		return
--- plugins/apod.lua
+++ plugins/apod.lua
@@ -1,12 +1,31 @@
 local apod = {}
 local HTTPS = require('ssl.https')
+local HTTP = require('socket.http')
 local JSON = require('dkjson')
 local URL = require('socket.url')
 local functions = require('functions')
 function apod:init(configuration)
 	apod.command = 'apod (YYYY/MM/DD)'
 	apod.triggers = functions.triggers(self.info.username, configuration.command_prefix):t('apod', true).table
+	apod.inline_triggers = apod.triggers
 	apod.doc = configuration.command_prefix .. 'apod (YYYY/MM/DD) - Sends the Astronomy Picture of the Day.'
+end
+function apod:inline_callback(inline_query, configuration)
+	local jstr = '[' .. HTTPS.request(configuration.apod_api .. configuration.apod_key) .. ']'
+	local jdat = JSON.decode(jstr)
+	local results = '['
+	local id = 600
+	for n in pairs(jdat) do
+		local title = jdat[n].title:gsub('"', '\\"')
+		results = results .. '{"type":"photo","id":"' .. id .. '","photo_url":"' .. jdat[n].url .. '","thumb_url":"' .. jdat[n].url .. '","caption":"' .. title .. '","reply_markup":{"inline_keyboard":[[{"text":"Read more", "url":"' .. jdat[n].url .. '"}]]}}'
+		id = id + 1
+		if n < #jdat then
+			results = results .. ','
+		end
+	end
+	local results = results .. ']'
+	print('test')
+	functions.answer_inline_query(inline_query, results, 1000)
 end
 function apod:action(msg, configuration)
 	local input = functions.input(msg.text)
@@ -23,9 +42,9 @@ function apod:action(msg, configuration)
 		functions.send_reply(msg, '`' .. configuration.errors.connection .. '`', true)
 		return
 	end
-	local data = JSON.decode(jstr)
-	local output = '"' .. data.title .. '" = ' .. data.date
+	local jdat = JSON.decode(jstr)
+	local title = '"' .. jdat.title .. '" = ' .. jdat.date
 	functions.send_action(msg.chat.id, 'upload_photo')
-	functions.send_photo(msg.chat.id, functions.download_to_file(data.url), output:gsub('-', '/'):gsub('=', '-'), msg.message_id)
+	functions.send_photo(msg.chat.id, functions.download_to_file(jdat.url), title:gsub('-', '/'):gsub('=', '-'), msg.message_id)
 end
 return apod
