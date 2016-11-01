21e55f41af6e7b8843988c5de5cc83d0f8e4b149
Matt <matthew@matthewhesketh.com>
2016-11-01 01:31:10 +0000

    mattata v3.2
    Improvements

--- plugins/github.lua
+++ plugins/github.lua
@@ -19,66 +19,34 @@ function github:onMessageReceive(msg, configuration)
 	end
 	local jstr = HTTPS.request(configuration.apis.github .. input)
 	local jdat = JSON.decode(jstr)
-	if jdat.id then
-		local full_name = ''
-		if jdat.full_name then
-			full_name = jdat.full_name
-		end
-		local html_url = ''
-		if jdat.html_url then
-			html_url = jdat.html_url
-		end
-		local language = jdat.language
-		local title = '[' .. full_name .. '](' .. html_url .. ') *|* ' .. language
-		local updated_at = ''
-		if jdat.updated_at then
-			updated_at = jdat.updated_at:gsub('T', ' '):gsub('Z', '')
-		end
-		local description = ''
-		if jdat.description then
-			description = jdat.description
-		end
-		local forks = ''
-		if jdat.forks_count then
-			forks = jdat.forks_count
-		end
-		local forks_count = ''
-		if tonumber(forks) == 1 then
-			forks_count = ' fork'
-		else
-			forks_count = ' forks'
-		end
-		local forks_url = html_url .. '/forks'
-		local watchers = ''
-		if jdat.watchers_count then
-			watchers = jdat.watchers_count
-		end
-		local watchers_count = ''
-		if tonumber(watchers) == 1 then
-			watchers_count = ' watcher'
-		else
-			watchers_count = ' watchers'
-		end
-		local watchers_url = html_url .. '/watchers'
-		local stargazers = ''
-		if jdat.stargazers_count then
-			stargazers = jdat.stargazers_count
-		end
-		local stargazers_count = ''
-		if tonumber(stargazers) == 1 then
-			stargazers_count = ' stargazer'
-		else
-			stargazers_count = ' stargazers'
-		end
-		local stargazers_url = html_url .. '/stargazers'
-		local stats = '[' .. forks .. forks_count .. '](' .. forks_url .. ') *|* [' .. watchers .. watchers_count .. '](' .. watchers_url .. ') *|* [' .. stargazers .. stargazers_count .. '](' .. stargazers_url .. ') \nLast updated at ' .. updated_at
-		local output = title .. '\n\n' .. '`' .. description .. '`' .. '\n\n' .. stats
-		mattata.sendMessage(msg.chat.id, output, 'Markdown', true, false, msg.message_id, nil)
-		return
-	else
+	if not jdat.id then
 		mattata.sendMessage(msg.chat.id, configuration.errors.results, nil, true, false, msg.message_id, nil)
 		return
 	end
+	local title = '[' .. jdat.full_name .. '](' .. jdat.html_url .. ') *|* ' .. jdat.language
+	local description, forks, stargazers, subscribers
+	if jdat.description then
+		description = '\n\n' .. '`' .. jdat.description .. '`' .. '\n\n'
+	else
+		description = '\n\n'
+	end
+	if jdat.forks_count == 1 then
+		forks = ' fork'
+	else
+		forks = ' forks'
+	end
+	if jdat.stargazers_count == 1 then
+		stargazers = ' star'
+	else
+		stargazers = ' stars'
+	end
+	if jdat.subscribers_count == 1 then
+		subscribers = ' watcher'
+	else
+		subscribers = ' watchers'
+	end
+	local output = title .. description .. '[' .. jdat.forks_count .. forks .. '](' .. jdat.html_url .. '/network) *|* [' .. jdat.stargazers_count .. stargazers .. '](' .. jdat.html_url .. '/stargazers) *|* [' .. jdat.subscribers_count .. subscribers .. '](' .. jdat.html_url .. '/watchers) \nLast updated at ' .. jdat.updated_at:gsub('T', ' '):gsub('Z', '')
+	mattata.sendMessage(msg.chat.id, output, 'Markdown', true, false, msg.message_id, nil)
 end
 
 return github
