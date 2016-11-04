ce82a056924e0c54a6e9e3b145439fe2b5b25ac0
Matthew Hesketh <matthew@matthewhesketh.com>
2016-11-04 13:30:29 +0000

    mattata v4
    Improvements in id.lua!
    
    - Inline results now return nice, Markdown-formatted messages

--- plugins/id.lua
+++ plugins/id.lua
@@ -6,7 +6,7 @@ function id:init()
 	id.arguments = 'id <user>'
 	id.commands = mattata.commands(self.info.username, configuration.commandPrefix):c('id').table
 	id.inlineCommands = id.commands
-	id.help = configuration.commandPrefix .. 'id <user> - Sends the name, ID, and (if applicable) username for the given user. Input is also accepted via reply. If no input is given, info about you is sent.'
+	id.help = configuration.commandPrefix .. 'id <user> - Sends the name, ID, and (if applicable) username for the given user. Input is also accepted via reply. If no input is given, info about you is sent. This command can also be used inline!'
 end
 
 function id:onInlineCallback(inline_query)
@@ -21,33 +21,33 @@ function id:onInlineCallback(inline_query)
 	local res = mattata.getChat(input)
 	if res then
 		if res.result.title then
-			output = 'Title: ' .. res.result.title .. '\nChat type: ' .. res.result.type:gsub('^%l', string.upper) .. '\nID: ' .. res.result.id
+			output = '*Title:* ' .. mattata.markdownEscape(res.result.title) .. '\n*Chat type:* ' .. res.result.type:gsub('^%l', string.upper) .. '\n*ID:* ' .. res.result.id
 			if res.result.participants_count then
-				output = output .. '\nMembers: ' .. res.result.participants_count
+				output = output .. '\n*Members:* ' .. res.result.participants_count
 			elseif res.result.username then
-				output = output .. '\nUsername: @' .. res.result.username
+				output = output .. '\n*Username:* @' .. mattata.markdownEscape(res.result.username)
 			end
 		else
-			name = res.result.first_name
+			name = mattata.markdownEscape(res.result.first_name)
 			id = res.result.id
 			if res.result.last_name then
-				name = name .. ' ' .. res.result.last_name
+				name = name .. ' ' .. mattata.markdownEscape(res.result.last_name)
 			end
 			if res.result.username then
-				username = res.result.username
-				output = 'Name: ' .. name .. '\nID: ' .. id .. '\nUsername: @' .. username
+				username = mattata.markdownEscape(res.result.username)
+				output = '*Name:* ' .. name .. '\n*ID:* ' .. id .. '\n*Username:* @' .. username
 			else
-				output = 'Name: ' .. name .. '\nID: ' .. id
+				output = '*Name:* ' .. name .. '\n*ID:* ' .. id
 			end
 			if res.result.when then
-				output = output .. '\nLast Seen: ' .. res.result.when
+				output = output .. '\n*Last Seen:* ' .. res.result.when
 			end
 		end
 	else
-		output = 'I don\'t recognise that username/ID.'
+		output = '*I don\'t recognise that username/ID.*'
 	end
-	local results = '[{"type":"article","id":"1","title":"/id","description":"' .. output .. '","input_message_content":{"message_text":"' .. output .. '"}}]'
-	mattata.answerInlineQuery(inline_query.id, results, 0)
+	local results = '[{"type":"article","id":"1","title":"/id","description":"' .. input .. '","input_message_content":{"message_text":"' .. output .. '","parse_mode":"Markdown"}}]'
+	mattata.answerInlineQuery(inline_query.id, '[' .. mattata.generateInlineArticle(1, configuration.commandPrefix .. 'id', output, 'Markdown', false, input) .. ']', 0)
 end
 
 function id:onMessageReceive(message)
