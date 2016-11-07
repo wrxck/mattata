85ac5b2e060e3b08442a6439942bed5c6692510b
Matthew Hesketh <matthew@matthewhesketh.com>
2016-11-07 15:17:15 +0000

    The YouTube plugin now returns multiple results for the given query
    There is no longer a link preview, and the results are Markdown-formatted

--- plugins/youtube.lua
+++ plugins/youtube.lua
@@ -14,27 +14,37 @@ local JSON = require('dkjson')
 function youtube:init(configuration)
 	youtube.arguments = 'youtube <query>'
 	youtube.commands = mattata.commands(self.info.username, configuration.commandPrefix):c('youtube'):c('yt').table
-	youtube.help = configuration.commandPrefix .. 'youtube <query> - Sends the first result from YouTube for the given search query. Alias: ' .. configuration.commandPrefix .. 'yt.'
+	youtube.help = configuration.commandPrefix .. 'youtube <query> - Sends the top results from YouTube for the given search query. Alias: ' .. configuration.commandPrefix .. 'yt.'
 end
 
 function youtube:onMessageReceive(message, configuration)
 	local input = mattata.input(message.text)
 	if not input then
-		mattata.sendMessage(message.chat.id, youtube.help, nil, true, false, message.message_id, nil)
+		mattata.sendMessage(message.chat.id, youtube.help, nil, true, false, message.message_id)
 		return
 	end
-	local jstr, res = HTTPS.request('https://www.googleapis.com/youtube/v3/search?key=' .. configuration.keys.google .. '&type=video&part=snippet&maxResults=1&q=' .. URL.escape(input))
+	local url
+	if message.chat.type == 'private' then
+		url = 'https://www.googleapis.com/youtube/v3/search?key=' .. configuration.keys.google .. '&type=video&part=snippet&maxResults=8&q=' .. URL.escape(input)
+	else
+		url = 'https://www.googleapis.com/youtube/v3/search?key=' .. configuration.keys.google .. '&type=video&part=snippet&maxResults=4&q=' .. URL.escape(input)
+	end
+	local jstr, res = HTTPS.request(url)
 	if res ~= 200 then
-		mattata.sendMessage(message.chat.id, configuration.errors.connection, nil, true, false, message.message_id, nil)
+		mattata.sendMessage(message.chat.id, configuration.errors.connection, nil, true, false, message.message_id)
 		return
 	end
 	local jdat = JSON.decode(jstr)
 	if jdat.pageInfo.totalResults == 0 then
-		mattata.sendMessage(message.chat.id, configuration.errors.results, nil, true, false, message.message_id, nil)
+		mattata.sendMessage(message.chat.id, configuration.errors.results, nil, true, false, message.message_id)
 		return
 	end
-	local output = 'https://www.youtube.com/watch?v=' .. jdat.items[1].id.videoId
-	mattata.sendMessage(message.chat.id, output, nil, false, false, message.message_id, nil)
+	local results = {}
+	for n in pairs(jdat.items) do
+		table.insert(results, '• [' .. mattata.markdownEscape(jdat.items[n].snippet.title) .. '](https://www.youtube.com/watch?v=' .. jdat.items[n].id.videoId .. ')')
+	end
+	local output = table.concat(results, '\n')
+	mattata.sendMessage(message.chat.id, output, 'Markdown', true, false, message.message_id)
 end
 
 return youtube
