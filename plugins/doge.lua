383b72f48ab1840ef39d746e94a39d825bcaa1bd
Matt <matthew@matthewhesketh.com>
2016-11-12 20:35:09 +0000

    Fixed a bug with doge.lua
    The plugin now matches against characters which can't be used as a
    parameter in the doge API url, and the help has been improved, with an
    example of usage.

--- plugins/doge.lua
+++ plugins/doge.lua
@@ -5,7 +5,7 @@ local URL = require('socket.url')
 function doge:init(configuration)
 	doge.arguments = 'doge <text>'
 	doge.commands = mattata.commands(self.info.username, configuration.commandPrefix):c('doge').table
-	doge.help = configuration.commandPrefix .. 'doge <text> - Doge-ifies the given text. It doesn\'t like emoji!'
+	doge.help = configuration.commandPrefix .. 'doge <text> - Doge-ifies the given text. Sentences are separated using slashes. Example: ' .. configuration.commandPrefix .. 'doge hello world/this is a test sentence'
 end
 
 function doge:onMessageReceive(message, configuration)
@@ -14,8 +14,15 @@ function doge:onMessageReceive(message, configuration)
 		mattata.sendMessage(message.chat.id, doge.help, nil, true, false, message.message_id, nil)
 		return
 	end
-	mattata.sendPhoto(message.chat.id, 'http://dogr.io/' .. input:gsub(' ', ''):gsub('\n', '/') .. '.png')
+	local url = 'http://dogr.io/' .. input:gsub(' ', '%%20') .. '.png?split=false&.png'
+	local matches = 'https?://[%%%w-_%.%?%.:/%+=&]+' -- Credit to @yagop for this snippet.
+	if string.match(url, matches) == url then
+		mattata.sendPhoto(message.chat.id, url)
+		return
+	else
+		mattata.sendMessage(message.chat.id, 'I could not generate an image with those parameters.', nil, true, false, message.message_id)
+		return
+	end
 end
 
 return doge
-
