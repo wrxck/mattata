ac47ddef3ffb53431d5e86584a17ab09a333464a
Matthew Hesketh <matthew@matthewhesketh.com>
2016-11-14 14:21:33 +0000

    More UX improvements for help.lua

--- plugins/help.lua
+++ plugins/help.lua
@@ -82,7 +82,7 @@ function help:onQueryReceive(callback, message)
 		helpStatistics = helpStatistics .. pluginCount .. '\n'
 		helpStatistics = helpStatistics .. punCount .. '\n'
 		helpStatistics = helpStatistics .. trumpCount
-		mattata.editMessageText(message.chat.id, message.message_id, helpStatistics, nil, true, '{"inline_keyboard":[[{"text":"Back", "callback_data":"helpBack"}]]}')
+		mattata.editMessageText(message.chat.id, message.message_id, helpStatistics .. '\n\nYou can view message statistics in groups I\'m part of, using the command \'' .. configuration.commandPrefix .. 'statistics\'.', nil, true, '{"inline_keyboard":[[{"text":"Back", "callback_data":"helpBack"}]]}')
 	end
 	if callback.data == 'helpCommands' then
 		if message.chat.type ~= 'private' then
@@ -101,7 +101,13 @@ function help:onQueryReceive(callback, message)
 		mattata.editMessageText(message.chat.id, message.message_id, helpLinks, 'Markdown', true, '{"inline_keyboard":[[{"text":"Official Group", "url":"https://telegram.me/mattata"},{"text":"Source Code", "url":"https://github.com/matthewhesketh/mattata"},{"text":"Rate Me", "url":"https://telegram.me/storebot?start=mattatabot"}],[{"text":"Back", "callback_data":"helpBack"}]]}')
 	end
 	if callback.data == 'helpBack' then
-		mattata.editMessageText(message.chat.id, message.message_id, '*Hello, ' .. mattata.markdownEscape(message.from.first_name) .. '!*\nMy name is ' .. self.info.first_name .. ' and I\'m an intelligent bot written with precision. There are many things I can do - try clicking the \'Commands\' button below to see what I can do for you.\n\n*Oh, and I work well in groups, too!*\nYou can enable and disable plugins in your group(s) using ' .. configuration.commandPrefix .. 'plugins.\n\n*Confused?*\nDon\'t worry, I was programmed to help! Try using ' .. configuration.commandPrefix .. 'help <command> to get help with a specific plugin and its usage.\n\nI\'m also an innovative example of artificial intelligence - yes, that\'s right; I can learn from you! Try speaking to me right here, or mention me by my name in a group. I can also describe images sent in response to messages I send.', 'Markdown', true, '{"inline_keyboard":[[{"text":"Links", "callback_data":"helpLinks"},{"text":"Statistics", "callback_data":"helpStatistics"},{"text":"Commands", "callback_data":"helpCommands"}]]}')
+		mattata.editMessageText(message.chat.id, message.message_id, '*Hello, ' .. mattata.markdownEscape(callback.from.first_name) .. '!*\nMy name is ' .. self.info.first_name .. ' and I\'m an intelligent bot written with precision. There are many things I can do - try clicking the \'Commands\' button below to see what I can do for you.\n\n*Oh, and I work well in groups, too!*\nYou can enable and disable plugins in your group(s) using ' .. configuration.commandPrefix .. 'plugins.', 'Markdown', true, '{"inline_keyboard":[[{"text":"Links", "callback_data":"helpLinks"},{"text":"Statistics", "callback_data":"helpStatistics"},{"text":"Commands", "callback_data":"helpCommands"}],[{"text":"Help", "callback_data":"helpHelp"},{"text":"About", "callback_data":"helpAbout"}]]}')
+	end
+	if callback.data == 'helpHelp' then
+		mattata.editMessageText(message.chat.id, message.message_id, '*Confused?*\nDon\'t worry, I was programmed to help! Try using ' .. configuration.commandPrefix .. 'help <command> to get help with a specific plugin and its usage.\n\nI\'m also an innovative example of artificial intelligence - yes, that\'s right; I can learn from you! Try speaking to me right here, or mention me by my name in a group. I can also describe images sent in response to messages I send.\n\nYou can also use me inline, try mentioning my username from any group and discover what else I can do!', 'Markdown', true, '{"inline_keyboard":[[{"text":"Back", "callback_data":"helpBack"}]]}')
+	end
+	if callback.data == 'helpAbout' then
+		mattata.editMessageText(message.chat.id, message.message_id, 'I\'m a bot written in Lua, and built to take advantage of the brilliant Bot API which Telegram offers.\n\nMy creator (and primary maintainer) is @wrxck.\nHe believes that anybody who enjoys programming should be able to work with the code of which I was compiled from, so I\'m proud to say that I am an open source project, which you can discover more about on [GitHub](https://github.com/matthewhesketh/mattata).', 'Markdown', true, '{"inline_keyboard":[[{"text":"Back", "callback_data":"helpBack"}]]}')
 	end
 end
 
@@ -116,10 +122,10 @@ function help:onMessageReceive(message)
 				return
 			end
 		end
-		mattata.sendMessage(message.chat.id, 'Sorry, there is no documented help for that plugin.', nil, true, false, message.message_id, nil)
-	else
-		mattata.sendMessage(message.chat.id, '*Hello, ' .. mattata.markdownEscape(message.from.first_name) .. '!*\nMy name is ' .. self.info.first_name .. ' and I\'m an intelligent bot written with precision. There are many things I can do - try clicking the \'Commands\' button below to see what I can do for you.\n\n*Oh, and I work well in groups, too!*\nYou can enable and disable plugins in your group(s) using ' .. configuration.commandPrefix .. 'plugins.\n\n*Confused?*\nDon\'t worry, I was programmed to help! Try using ' .. configuration.commandPrefix .. 'help <command> to get help with a specific plugin and its usage.\n\nI\'m also an innovative example of artificial intelligence - yes, that\'s right; I can learn from you! Try speaking to me right here, or mention me by my name in a group. I can also describe images sent in response to messages I send.', 'Markdown', true, false, nil, '{"inline_keyboard":[[{"text":"Links", "callback_data":"helpLinks"},{"text":"Statistics", "callback_data":"helpStatistics"},{"text":"Commands", "callback_data":"helpCommands"}]]}')
+		mattata.sendMessage(message.chat.id, 'I\'m sorry, but I\'m afraid there is no help documented for that plugin at this moment in time. If you believe this is a mistake, please don\'t hesitate to contact [my developer](https://telegram.me/wrxck).', nil, true, false, message.message_id, nil)
+		return
 	end
+	mattata.sendMessage(message.chat.id, '*Hello, ' .. mattata.markdownEscape(message.from.first_name) .. '!*\nMy name is ' .. self.info.first_name .. ' and I\'m an intelligent bot written with precision. There are many things I can do - try clicking the \'Commands\' button below to see what I can do for you.\n\n*Oh, and I work well in groups, too!*\nYou can enable and disable plugins in your group(s) using ' .. configuration.commandPrefix .. 'plugins.', 'Markdown', true, false, nil, '{"inline_keyboard":[[{"text":"Links", "callback_data":"helpLinks"},{"text":"Statistics", "callback_data":"helpStatistics"},{"text":"Commands", "callback_data":"helpCommands"}],[{"text":"Help", "callback_data":"helpHelp"},{"text":"About", "callback_data":"helpAbout"}]]}')
 end
 
 return help
