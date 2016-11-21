2a06eca1db179fc6c15221a0f377640abd629e19
Matthew Hesketh <matthew@matthewhesketh.com>
2016-11-21 15:12:14 +0000

    Channel support

--- plugins/coinflip.lua
+++ plugins/coinflip.lua
@@ -7,39 +7,58 @@ function coinflip:init(configuration)
 	coinflip.help = configuration.commandPrefix .. 'coinflip <guess> - Flips a coin and returns the result! If no arguments are given, the result of a random coin flip is returned; if, however, an argument is given, the result of the random coin flip tests against your guess and returns the result and whether your guess was correct. Alias: ' .. configuration.commandPrefix .. 'cf.'
 end
 
-function coinflip:onMessageReceive(message)
-	local input = mattata.input(message.text)
+function coinflip:onChannelPostReceive(channel_post)
+	local input = mattata.input(channel_post.text_lower)
 	if input then
-		local guess = input:gsub('heads', '1'):gsub('Heads', '1'):gsub('h', '1'):gsub('H', '1'):gsub('tails', '2'):gsub('Tails', '2'):gsub('t', 2):gsub('T', '2')
 		local flip = math.random(2)
-		local result = ''
-		local output = ''
+		local result
 		if flip == 1 then
 			result = 'Heads.'
 		else
 			result = 'Tails.'
 		end
-		if tonumber(guess) == flip then
-			output = '*The coin landed on:* ' .. result .. ' _You were correct!_'
-			mattata.sendMessage(message.chat.id, output, 'Markdown', true, false, message.message_id, nil)
-			return
-		else
-			output = '*The coin landed on:* ' .. result .. ' _You weren\'t correct, try again!_'
-			mattata.sendMessage(message.chat.id, output, 'Markdown', true, false, message.message_id, nil)
+		if tonumber(input:gsub('heads', '1'):gsub('h', '1'):gsub('tails', '2'):gsub('t', 2)) == flip then
+			mattata.sendMessage(channel_post.chat.id, '*The coin landed on:* ' .. result .. ' _You were correct!_', 'Markdown', true, false, channel_post.message_id)
 			return
 		end
-	else
-		local flip = math.random(2)
-		local result = ''
-		if flip == 1 then
-			result = 'Heads.'
-		else
-			result = 'Tails.'
-		end
-		local output = '*The coin landed on:* ' .. result
-		mattata.sendMessage(message.chat.id, output, 'Markdown', true, false, message.message_id, nil)
+		mattata.sendMessage(channel_post.chat.id, '*The coin landed on:* ' .. result .. ' _You weren\'t correct, try again!_', 'Markdown', true, false, channel_post.message_id)
 		return
 	end
+	local flip = math.random(2)
+	local result
+	if flip == 1 then
+		result = 'Heads.'
+	else
+		result = 'Tails.'
+	end
+	mattata.sendMessage(channel_post.chat.id, '*The coin landed on:* ' .. result, 'Markdown', true, false, channel_post.message_id)
+end
+
+function coinflip:onMessageReceive(message)
+	local input = mattata.input(message.text_lower)
+	if input then
+		local flip = math.random(2)
+		local result
+		if flip == 1 then
+			result = 'Heads.'
+		else
+			result = 'Tails.'
+		end
+		if tonumber(input:gsub('heads', '1'):gsub('h', '1'):gsub('tails', '2'):gsub('t', 2)) == flip then
+			mattata.sendMessage(message.chat.id, '*The coin landed on:* ' .. result .. ' _You were correct!_', 'Markdown', true, false, message.message_id)
+			return
+		end
+		mattata.sendMessage(message.chat.id, '*The coin landed on:* ' .. result .. ' _You weren\'t correct, try again!_', 'Markdown', true, false, message.message_id)
+		return
+	end
+	local flip = math.random(2)
+	local result
+	if flip == 1 then
+		result = 'Heads.'
+	else
+		result = 'Tails.'
+	end
+	mattata.sendMessage(message.chat.id, '*The coin landed on:* ' .. result, 'Markdown', true, false, message.message_id)
 end
 
 return coinflip
