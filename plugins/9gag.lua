64a0a8a5fcb1cbd271649d72d63447289f67c17c
Matthew Hesketh <matthew@matthewhesketh.com>
2016-11-21 14:05:14 +0000

    Implemented framework changes
    Also added better inline results and implemented the new language mode

--- plugins/9gag.lua
+++ plugins/9gag.lua
@@ -1,7 +1,7 @@
 local ninegag = {}
+local mattata = require('mattata')
 local HTTP = require('socket.http')
 local JSON = require('dkjson')
-local mattata = require('mattata')
 
 function ninegag:init(configuration)
 	ninegag.arguments = '9gag'
@@ -10,37 +10,61 @@ function ninegag:init(configuration)
 	ninegag.help = configuration.commandPrefix .. '9gag - Returns a random result from the latest 9gag images.'
 end
 
-function ninegag:onInlineCallback(inline_query, configuration)
-	local jstr = HTTP.request(configuration.apis.ninegag)
-	local jdat = JSON.decode(jstr)
-	local results = '['
-	local id = 1
-	for n in pairs(jdat) do
-		local title = jdat[n].title:gsub('"', '\\"')
-		results = results .. mattata.generateInlinePhoto(id, jdat[n].src, jdat[n].src, nil, nil, title, 'Read more', jdat[n].url)
-		id = id + 1
-		if n < #jdat then
-			results = results .. ','
-		end
-	end
-	local results = results .. ']'
-	mattata.answerInlineQuery(inline_query.id, results, 0)
-end
-
-function ninegag:onMessageReceive(message, configuration)
-	local url = configuration.apis.ninegag
-	local jstr, res = HTTP.request(url)
+function ninegag:onInlineCallback(inline_query, language)
+	local jstr, res = HTTP.request('http://api-9gag.herokuapp.com/')
 	if res ~= 200 then
-		mattata.sendMessage(message.chat.id, configuration.errors.connection, nil, true, false, message.message_id)
+		local results = JSON.encode({
+			{
+				type = 'article',
+				id = '1',
+				title = 'An error occured!',
+				description = language.errors.connection,
+				input_message_content = {
+					message_text = language.errors.connection
+				}
+			}
+		})
+		mattata.answerInlineQuery(inline_query.id, results, 0)
 		return
 	end
 	local jdat = JSON.decode(jstr)
-	local jrnd = math.random(#jdat)
-	local link_image = jdat[jrnd].src
-	local title = jdat[jrnd].title
-	local post_url = jdat[jrnd].url
+	local resultsList = {}
+	local resultId = 1
+	for n in pairs(jdat) do
+		if jdat[n].src and jdat[n].title then
+			local result = {
+				type = 'photo',
+				id = tostring(resultId),
+				photo_url = jdat[n].src,
+				thumb_url = jdat[n].src,
+				caption = jdat[n].title:gsub('"', '\\"')
+			}
+			table.insert(resultsList, result)
+		end
+		resultId = resultId + 1
+	end
+	mattata.answerInlineQuery(inline_query.id, JSON.encode(resultsList), 0)
+end
+
+function ninegag:onMessageReceive(message, language)
+	local jstr, res = HTTP.request('http://api-9gag.herokuapp.com/')
+	if res ~= 200 then
+		mattata.sendMessage(message.chat.id, language.errors.connection, nil, true, false, message.message_id)
+		return
+	end
 	mattata.sendChatAction(message.chat.id, 'upload_photo')
-	mattata.sendPhoto(message.chat.id, link_image, title, false, message.message_id, '{"inline_keyboard":[[{"text":"Read more", "url":"' .. post_url .. '"}]]}')
+	local jdat = JSON.decode(jstr)
+	local jrnd = math.random(#jdat)
+	local keyboard = {}
+	keyboard.inline_keyboard = {
+		{
+			{
+				text = 'Read more',
+				url = jdat[jrnd].url
+			}
+		}
+	}
+	mattata.sendPhoto(message.chat.id, jdat[jrnd].src, jdat[jrnd].title, false, message.message_id, JSON.encode(keyboard))
 end
 
 return ninegag
