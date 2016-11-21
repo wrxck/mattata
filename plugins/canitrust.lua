947f2e382af992ddf1a6bafc37dec985521a47ee
Matthew Hesketh <matthew@matthewhesketh.com>
2016-11-21 15:05:53 +0000

    Language and channel support

--- plugins/canitrust.lua
+++ plugins/canitrust.lua
@@ -12,14 +12,14 @@ function canitrust:init(configuration)
 end
 
 function canitrust.validate(url)
-	local parsed_url = URL.parse(url, { scheme = 'http', authority = '' })
-	if not parsed_url.host and parsed_url.path then
-		parsed_url.host = parsed_url.path
-		parsed_url.path = ''
+	local parsedUrl = URL.parse(url, { scheme = 'http', authority = '' })
+	if not parsedUrl.host and parsedUrl.path then
+		parsedUrl.host = parsedUrl.path
+		parsedUrl.path = ''
 	end
-	local url = URL.build(parsed_url)
+	local url = URL.build(parsedUrl)
 	local protocol
-	if parsed_url.scheme == 'http' then
+	if parsedUrl.scheme == 'http' then
 		protocol = HTTP
 	end
 	local options = {
@@ -34,71 +34,141 @@ function canitrust.validate(url)
 	end
 	return true
 end
-function canitrust:onMessageReceive(message, configuration)
-	local input = mattata.input(message.text)
-	if input then
-		input = input:gsub('https', 'http'):gsub('HTTPS', 'https'):gsub('HTTP', 'http'):gsub('www.', '')
-		local jstr = HTTPS.request(configuration.apis.canitrust .. URL.escape(input) .. '/&callback=process&key=' .. configuration.keys.canitrust)
-		local jdat = JSON.decode(jstr)
-		local output = ''
-		if not canitrust.validate(input) then
-			output = 'Invalid URL.'
-		end
-		if jstr == 'process({ "' .. input .. '": { "target": "' .. input .. '" } } )' and canitrust.validate(input) then
-			output = 'There are *no known issues* with this website.'
-		end
-		if string.match(jstr, '"101"') then
-			output = 'This website is likely to contain *malware*.'
-		elseif string.match(jstr, '"102"') then
-			output = 'This website is likely to provide a *poor customer experience*.'
-		elseif string.match(jstr, '"103"') then
-			output = 'This website has been flagged as *phishing*.'
-		elseif string.match(jstr, '"104"') then
-			output = 'This website has been flagged as *a scam*.'
-		elseif string.match(jstr, '"105"') then
-			output = 'This website is *potentially illegal*.'
-		elseif string.match(jstr, '"201"') then
-			output = 'This website is known to be *unethical*, and may provide *misleading claims*.'
-		elseif string.match(jstr, '"202"') then
-			output = 'This website has been flagged as a *privacy risk*.'
-		elseif string.match(jstr, '"203"') then
-			output = 'This website is *suspicious*.'
-		elseif string.match(jstr, '"204"') then
-			output = 'This website has been flagged for containing *hate/discrimination*.'
-		elseif string.match(jstr, '"205"') then
-			output = 'This website has been flagged as *spam*.'
-		elseif string.match(jstr, '"206"') then
-			output = 'This website has been known to distribute *potentially unwanted programs*.'
-		elseif string.match(jstr, '"207"') then
-			output = 'This website contains *ads/pop-ups*.'
-		elseif string.match(jstr, '"301"') then
-			output = 'This website is known to *track your online activity*.'
-		elseif string.match(jstr, '"302"') then
-			output = 'This website has been associated with *alternative or controversial medicine*.'
-		elseif string.match(jstr, '"303"') then
-			output = 'This website is likely to contain *religious/political beliefs*.'
-		elseif string.match(jstr, '"401"') then
-			output = 'This website contains *adult content*.'
-		elseif string.match(jstr, '"402"') then
-			output = 'This website contains *incidental nudity*.'
-		elseif string.match(jstr, '"403"') then
-			output = 'This website has been flagged as *gruesome or shocking*.'
-		elseif string.match(jstr, '"404"') then
-			output = 'This website is *suitable for kids*.'
-		elseif string.match(jstr, '"501"') then
-			output = 'There are *no known issues* with this website.'
-		end
-		if output ~= 'Invalid URL.' then
-			mattata.sendMessage(message.chat.id, output, 'Markdown', true, false, message.message_id, '{"inline_keyboard":[[{"text":"' .. 'Proceed to site' .. '", "url":"' .. input .. '"}]]}')
-			return
-		else
-			mattata.sendMessage(message.chat.id, output, 'Markdown', true, false, message.message_id, nil)
-			return
-		end
-	else
-		mattata.sendMessage(message.chat.id, canitrust.help, nil, true, false, message.message_id, nil)
+
+function canitrust:onChannelPostReceive(channel_post, configuration)
+	local input = mattata.input(channel_post.text_lower)
+	if not input then
+		mattata.sendMessage(channel_post.chat.id, canitrust.help, nil, true, false, channel_post.message_id)
 		return
 	end
+	local jstr = HTTPS.request('https://api.mywot.com/0.4/public_link_json2?hosts=' .. URL.escape(input) .. '/&callback=process&key=' .. configuration.keys.canitrust)
+	local jdat = JSON.decode(jstr)
+	local output = ''
+	if not canitrust.validate(input) then
+		mattata.sendMessage(channel_post.chat.id, configuration.errors.results, nil, true, false, channel_post.message_id)
+		return
+	end
+	if jstr:match('^process%({ "' .. input .. '": { "target": "' .. input .. '" } } %)$') then
+		output = 'There are *no known issues* with this website.'
+	elseif string.match(jstr, '"101"') then
+		output = 'This website is likely to contain *malware*.'
+	elseif string.match(jstr, '"102"') then
+		output = 'This website is likely to provide a *poor customer experience*.'
+	elseif string.match(jstr, '"103"') then
+		output = 'This website has been flagged as *phishing*.'
+	elseif string.match(jstr, '"104"') then
+		output = 'This website has been flagged as *a scam*.'
+	elseif string.match(jstr, '"105"') then
+		output = 'This website is *potentially illegal*.'
+	elseif string.match(jstr, '"201"') then
+		output = 'This website is known to be *unethical*, and may provide *misleading claims*.'
+	elseif string.match(jstr, '"202"') then
+		output = 'This website has been flagged as a *privacy risk*.'
+	elseif string.match(jstr, '"203"') then
+		output = 'This website is *suspicious*.'
+	elseif string.match(jstr, '"204"') then
+		output = 'This website has been flagged for containing *hate/discrimination*.'
+	elseif string.match(jstr, '"205"') then
+		output = 'This website has been flagged as *spam*.'
+	elseif string.match(jstr, '"206"') then
+		output = 'This website has been known to distribute *potentially unwanted programs*.'
+	elseif string.match(jstr, '"207"') then
+		output = 'This website contains *ads/pop-ups*.'
+	elseif string.match(jstr, '"301"') then
+		output = 'This website is known to *track your online activity*.'
+	elseif string.match(jstr, '"302"') then
+		output = 'This website has been associated with *alternative or controversial medicine*.'
+	elseif string.match(jstr, '"303"') then
+		output = 'This website is likely to contain *religious/political beliefs*.'
+	elseif string.match(jstr, '"401"') then
+		output = 'This website contains *adult content*.'
+	elseif string.match(jstr, '"402"') then
+		output = 'This website contains *incidental nudity*.'
+	elseif string.match(jstr, '"403"') then
+		output = 'This website has been flagged as *gruesome or shocking*.'
+	elseif string.match(jstr, '"404"') then
+		output = 'This website is *suitable for kids*.'
+	elseif string.match(jstr, '"501"') then
+		output = 'There are *no known issues* with this website.'
+	end
+	local keyboard = {}
+	keyboard.inline_keyboard = {
+		{
+			{
+				text = 'Proceed to site',
+				url = input
+			}
+		}
+	}
+	mattata.sendMessage(channel_post.chat.id, output, 'Markdown', true, false, channel_post.message_id, JSON.encode(keyboard))
+end
+
+function canitrust:onMessageReceive(message, configuration, language)
+	local input = mattata.input(message.text_lower)
+	if not input then
+		mattata.sendMessage(message.chat.id, canitrust.help, nil, true, false, message.message_id)
+		return
+	end
+	local jstr = HTTPS.request('https://api.mywot.com/0.4/public_link_json2?hosts=' .. URL.escape(input) .. '/&callback=process&key=' .. configuration.keys.canitrust)
+	local jdat = JSON.decode(jstr)
+	local output = ''
+	if not canitrust.validate(input) then
+		mattata.sendMessage(message.chat.id, language.errors.results, nil, true, false, message.message_id)
+		return
+	end
+	if jstr:match('^process%({ "' .. input .. '": { "target": "' .. input .. '" } } %)$') then
+		output = 'There are *no known issues* with this website.'
+	elseif string.match(jstr, '"101"') then
+		output = 'This website is likely to contain *malware*.'
+	elseif string.match(jstr, '"102"') then
+		output = 'This website is likely to provide a *poor customer experience*.'
+	elseif string.match(jstr, '"103"') then
+		output = 'This website has been flagged as *phishing*.'
+	elseif string.match(jstr, '"104"') then
+		output = 'This website has been flagged as *a scam*.'
+	elseif string.match(jstr, '"105"') then
+		output = 'This website is *potentially illegal*.'
+	elseif string.match(jstr, '"201"') then
+		output = 'This website is known to be *unethical*, and may provide *misleading claims*.'
+	elseif string.match(jstr, '"202"') then
+		output = 'This website has been flagged as a *privacy risk*.'
+	elseif string.match(jstr, '"203"') then
+		output = 'This website is *suspicious*.'
+	elseif string.match(jstr, '"204"') then
+		output = 'This website has been flagged for containing *hate/discrimination*.'
+	elseif string.match(jstr, '"205"') then
+		output = 'This website has been flagged as *spam*.'
+	elseif string.match(jstr, '"206"') then
+		output = 'This website has been known to distribute *potentially unwanted programs*.'
+	elseif string.match(jstr, '"207"') then
+		output = 'This website contains *ads/pop-ups*.'
+	elseif string.match(jstr, '"301"') then
+		output = 'This website is known to *track your online activity*.'
+	elseif string.match(jstr, '"302"') then
+		output = 'This website has been associated with *alternative or controversial medicine*.'
+	elseif string.match(jstr, '"303"') then
+		output = 'This website is likely to contain *religious/political beliefs*.'
+	elseif string.match(jstr, '"401"') then
+		output = 'This website contains *adult content*.'
+	elseif string.match(jstr, '"402"') then
+		output = 'This website contains *incidental nudity*.'
+	elseif string.match(jstr, '"403"') then
+		output = 'This website has been flagged as *gruesome or shocking*.'
+	elseif string.match(jstr, '"404"') then
+		output = 'This website is *suitable for kids*.'
+	elseif string.match(jstr, '"501"') then
+		output = 'There are *no known issues* with this website.'
+	end
+	local keyboard = {}
+	keyboard.inline_keyboard = {
+		{
+			{
+				text = 'Proceed to site',
+				url = input
+			}
+		}
+	}
+	mattata.sendMessage(message.chat.id, output, 'Markdown', true, false, message.message_id, JSON.encode(keyboard))
 end
 
 return canitrust
