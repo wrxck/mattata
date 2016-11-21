1818272981adbe8c20001bb60d1991734487ac3d
Matthew Hesketh <matthew@matthewhesketh.com>
2016-11-21 14:54:10 +0000

    Moved names from configuration to plugin

--- plugins/bandersnatch.lua
+++ plugins/bandersnatch.lua
@@ -15,11 +15,189 @@ function bandersnatch:init(configuration)
 	bandersnatch.help = configuration.commandPrefix .. 'bandersnatch - Generates a fun, tongue-twisting name.'
 end
 
-function bandersnatch:onInlineCallback(inline_query, configuration)
+local fullNames = {
+	'Wimbledon Tennismatch',
+	'Rinkydink Curdlesnoot',
+	'Butawhiteboy Cantbekhan',
+	'Benadryl Claritin',
+	'Bombadil Rivendell',
+	'Wanda Crotchfruit',
+	'Biblical Concubine',
+	'Syphilis Cankersore',
+	'Buckminster Fullerene',
+	'Bourgeoisie Capitalist'
+}
+
+local firstNames = {
+	'Bumblebee',
+	'Bandersnatch',
+	'Broccoli',
+	'Rinkydink',
+	'Bombadil',
+	'Boilerdang',
+	'Bandicoot',
+	'Fragglerock',
+	'Muffintop',
+	'Congleton',
+	'Blubberdick',
+	'Buffalo',
+	'Benadryl',
+	'Butterfree',
+	'Burberry',
+	'Whippersnatch',
+	'Buttermilk',
+	'Beezlebub',
+	'Budapest',
+	'Boilerdang',
+	'Blubberwhale',
+	'Bumberstump',
+	'Bulbasaur',
+	'Cogglesnatch',
+	'Liverswort',
+	'Bodybuild',
+	'Johnnycash',
+	'Bendydick',
+	'Burgerking',
+	'Bonaparte',
+	'Bunsenburner',
+	'Billiardball',
+	'Bukkake',
+	'Baseballmitt',
+	'Blubberbutt',
+	'Baseballbat',
+	'Rumblesack',
+	'Barister',
+	'Danglerack',
+	'Rinkydink',
+	'Bombadil',
+	'Honkytonk',
+	'Billyray',
+	'Bumbleshack',
+	'Snorkeldink',
+	'Anglerfish',
+	'Beetlejuice',
+	'Bedlington',
+	'Bandicoot',
+	'Boobytrap',
+	'Blenderdick',
+	'Bentobox',
+	'Anallube',
+	'Pallettown',
+	'Wimbledon',
+	'Buttercup',
+	'Blasphemy',
+	'Snorkeldink',
+	'Brandenburg',
+	'Barbituate',
+	'Snozzlebert',
+	'Tiddleywomp',
+	'Bouillabaisse',
+	'Wellington',
+	'Benetton',
+	'Bendandsnap',
+	'Timothy',
+	'Brewery',
+	'Bentobox',
+	'Brandybuck',
+	'Benjamin',
+	'Buckminster',
+	'Bourgeoisie',
+	'Bakery',
+	'Oscarbait',
+	'Buckyball',
+	'Bourgeoisie',
+	'Burlington',
+	'Buckingham',
+	'Barnoldswick'
+}
+
+local lastNames = {
+	'Coddleswort',
+	'Crumplesack',
+	'Curdlesnoot',
+	'Calldispatch',
+	'Humperdinck',
+	'Rivendell',
+	'Cuttlefish',
+	'Lingerie',
+	'Vegemite',
+	'Ampersand',
+	'Cumberbund',
+	'Candycrush',
+	'Clombyclomp',
+	'Cragglethatch',
+	'Nottinghill',
+	'Cabbagepatch',
+	'Camouflage',
+	'Creamsicle',
+	'Curdlemilk',
+	'Upperclass',
+	'Frumblesnatch',
+	'Crumplehorn',
+	'Talisman',
+	'Candlestick',
+	'Chesterfield',
+	'Bumbersplat',
+	'Scratchnsniff',
+	'Snugglesnatch',
+	'Charizard',
+	'Carrotstick',
+	'Cumbercooch',
+	'Crackerjack',
+	'Crucifix',
+	'Cuckatoo',
+	'Cockletit',
+	'Collywog',
+	'Capncrunch',
+	'Covergirl',
+	'Cumbersnatch',
+	'Countryside',
+	'Coggleswort',
+	'Splishnsplash',
+	'Copperwire',
+	'Animorph',
+	'Curdledmilk',
+	'Cheddarcheese',
+	'Cottagecheese',
+	'Crumplehorn',
+	'Snickersbar',
+	'Banglesnatch',
+	'Stinkyrash',
+	'Cameltoe',
+	'Chickenbroth',
+	'Concubine',
+	'Candygram',
+	'Moldyspore',
+	'Chuckecheese',
+	'Cankersore',
+	'Crimpysnitch',
+	'Wafflesmack',
+	'Chowderpants',
+	'Toodlesnoot',
+	'Clavichord',
+	'Cuckooclock',
+	'Oxfordshire',
+	'Cumbersome',
+	'Chickenstrips',
+	'Battleship',
+	'Commonwealth',
+	'Cunningsnatch',
+	'Custardbath',
+	'Kryptonite',
+	'Curdlesnoot',
+	'Cummerbund',
+	'Coochyrash',
+	'Crackerdong',
+	'Curdledong',
+	'Crackersprout',
+	'Crumplebutt',
+	'Colonist',
+	'Coochierash',
+	'Thundersnatch'
+}
+
+function bandersnatch:onInlineCallback(inline_query)
 	local output
-	local fullNames = configuration.bandersnatch.fullNames
-	local firstNames = configuration.bandersnatch.firstNames
-	local lastNames = configuration.bandersnatch.lastNames
 	if math.random(10) == 10 then
 		output = fullNames[math.random(#fullNames)]
 	else
@@ -28,14 +206,22 @@ function bandersnatch:onInlineCallback(inline_query, configuration)
 	mattata.answerInlineQuery(inline_query.id, '[' .. mattata.generateInlineArticle(1, output, output, 'Markdown', false, 'Click to send your new name!') .. ']', 0)
 end
 
-function bandersnatch:onMessageReceive(message, configuration)
-	local fullNames = configuration.bandersnatch.fullNames
-	local firstNames = configuration.bandersnatch.firstNames
-	local lastNames = configuration.bandersnatch.lastNames
+function bandersnatch:onChannelPostReceive(channel_post)
+	local output
 	if math.random(10) == 10 then
-		local output = fullNames[math.random(#fullNames)]
+		output = fullNames[math.random(#fullNames)]
 	else
-		local output = firstNames[math.random(#firstNames)] .. ' ' .. lastNames[math.random(#lastNames)]
+		output = firstNames[math.random(#firstNames)] .. ' ' .. lastNames[math.random(#lastNames)]
+	end
+	mattata.sendMessage(channel_post.chat.id, output, nil, true, false, channel_post.message_id)
+end
+
+function bandersnatch:onMessageReceive(message)
+	local output
+	if math.random(10) == 10 then
+		output = fullNames[math.random(#fullNames)]
+	else
+		output = firstNames[math.random(#firstNames)] .. ' ' .. lastNames[math.random(#lastNames)]
 	end
 	mattata.sendMessage(message.chat.id, output, nil, true, false, message.message_id)
 end
