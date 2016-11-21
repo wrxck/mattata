d7407a5376665b4d894abf232302259d1e843aa4
Matthew Hesketh <matthew@matthewhesketh.com>
2016-11-21 15:22:01 +0000

    Update currency.lua

--- plugins/currency.lua
+++ plugins/currency.lua
@@ -6,21 +6,19 @@
 ]]--
 
 local currency = {}
-local HTTPS = require('ssl.https')
 local mattata = require('mattata')
+local HTTPS = require('ssl.https')
 
-function currency:init()
-	local configuration = require('configuration')
+function currency:init(configuration)
 	currency.arguments = 'currency <amount> <from> TO <to>'
 	currency.commands = mattata.commands(self.info.username, configuration.commandPrefix):c('currency').table
 	currency.help = configuration.commandPrefix .. 'currency <amount> <from> TO <to> - Converts exchange rates for various currencies. Source: Google Finance.'
 end
 
-function currency:onMessageReceive(message)
-	local configuration = require('configuration')
+function currency:onMessageReceive(message, language)
 	local input = message.text:upper()
 	if not input:match('%a%a%a TO %a%a%a') then
-		mattata.sendMessage(message.chat.id, currency.help, nil, true, false, message.message_id, nil)
+		mattata.sendMessage(message.chat.id, currency.help, nil, true, false, message.message_id)
 		return
 	end
 	local from = input:match('(%a%a%a) TO')
@@ -28,17 +26,17 @@ function currency:onMessageReceive(message)
 	local amount = mattata.getWord(input, 2)
 	amount = tonumber(amount) or 1
 	local result = 1
-	local api = configuration.apis.currency
+	local api = 'https://www.google.com/finance/converter'
 	if from ~= to then
 		api = api .. '?from=' .. from .. '&to=' .. to .. '&a=' .. amount
 		local str, res = HTTPS.request(api)
 		if res ~= 200 then
-			mattata.sendMessage(message.chat.id, configuration.errors.connection, nil, true, false, message.message_id, nil)
+			mattata.sendMessage(message.chat.id, language.errors.connection, nil, true, false, message.message_id)
 			return
 		end
 		str = str:match('<span class=bld>(.*) %u+</span>')
 		if not str then
-			mattata.sendMessage(message.chat.id, configuration.errors.results, nil, true, false, message.message_id, nil)
+			mattata.sendMessage(message.chat.id, language.errors.results, nil, true, false, message.message_id)
 			return
 		end
 		result = string.format('%.2f', str)
@@ -46,7 +44,7 @@ function currency:onMessageReceive(message)
 	local output = amount .. ' ' .. from .. ' = ' .. result .. ' ' .. to .. '\n\n'
 	output = output .. os.date('!%F %T UTC') .. '\nSource: Google Finance'
 	output = '\n' .. output .. '\n'
-	mattata.sendMessage(message.chat.id, output, nil, true, false, message.message_id, nil)
+	mattata.sendMessage(message.chat.id, output, nil, true, false, message.message_id)
 end
 
 return currency
