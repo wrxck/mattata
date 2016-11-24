4237e4d47fbc5584cca7720e6ae89ee8ac9b583d
Matthew Hesketh <matthew@matthewhesketh.com>
2016-11-24 09:00:56 +0000

    [v5.2] Slight adjustments to the launch script

--- launch.sh
+++ launch.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 if [ ! -f ./configuration.lua ]
 then
-	echo "Please insert the required variables into configuration.example.lua."
-    echo "Then, you need to rename configuration.example.lua to configuration.lua!"
+	echo "[mattata] Please insert the required variables into configuration.example.lua."
+    echo "[mattata] Then, you need to rename configuration.example.lua to configuration.lua!"
 else
 	while true; do
 		lua -e "require('mattata').run({}, require('configuration'))"
-		echo 'mattata has stopped.'
+		echo '[mattata] Your instance of mattata has been stopped.'
 		sleep 3s
 	done
 fi
