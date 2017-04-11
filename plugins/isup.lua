e1c5cc2881f7e244bdd527d2785f9e37b2ac8fcb
Matt <wrxck0@gmail.com>
2017-04-11 14:00:38 +0100

    Addressed issues #22 and #23
    isup.lua now uses an external API because of the highlighted security
    risks and their potential implications.

--- plugins/isup.lua
+++ plugins/isup.lua
@@ -1,54 +1,49 @@
 --[[
-    Copyright 2017 wrxck <matthew@matthewhesketh.com>
+    Copyright 2017 Matthew Hesketh <wrxck0@gmail.com>
     This code is licensed under the MIT. See LICENSE for details.
 ]]
 
 local isup = {}
-
 local mattata = require('mattata')
-local https = require('ssl.https')
 local http = require('socket.http')
 local url = require('socket.url')
 
 function isup:init()
-    isup.commands = mattata.commands(
-        self.info.username
-    ):command('isup').table
-    isup.help = [[/isup <url> - Checks to see if the given URL is down for everyone or just you.]]
-end
-
-function isup.is_site_up(input)
-    local protocol = http
-    if input:lower():match('^https') then
-        protocol = https
-    elseif not input:lower():match('^http') then
-        input = 'http://' .. input
-    end
-    local _, res = protocol.request(input)
-    res = tonumber(res)
-    if not res or res > 399 then
-        return false
-    end
-    return true
+    isup.commands = mattata.commands(self.info.username):command('isup').table
+    isup.help = '/isup <url> - Checks to see if the given URL is down for everyone or just you.'
 end
 
 function isup:on_message(message, configuration)
     local input = mattata.input(message.text)
-    if not input then
+    if not input
+    then
         return mattata.send_reply(
             message,
             isup.help
         )
     end
-    if isup.is_site_up(input) then
+    local str, res = http.request('http://isup.me/' .. url.escape(input))
+    if res ~= 200
+    then
         return mattata.send_reply(
             message,
-            [[This website is up, maybe it's just you?]]
+            configuration.errors.connection
         )
+    end
+    local output = configuration.errors.connection
+    if str:match('It\'s just you.')
+    then
+        output = 'This website appears to be up, maybe it\'s just you?'
+    elseif str:match('doesn\'t look like a site')
+    then
+        output = 'That doesn\'t appear to be a valid site!'
+    elseif str:match('looks down from here')
+    then
+        output = 'It\'s not just you, this website looks down from here.'
     end
     return mattata.send_reply(
         message,
-        [[It's not just you, this website is down!]]
+        output
     )
 end
 
