ed70c7d8a770e25f8246a16363c41373ab910351
xxdamage <xxxdamagee@gmail.com>
2017-09-12 18:22:03 +0200

    Update unfilter.lua
    you can add phrases or words, but the match will take a set of words (phrase) as one. In my opinion, it is more useful in this way

--- plugins/unfilter.lua
+++ plugins/unfilter.lua
@@ -32,7 +32,7 @@ function unfilter:on_message(message, configuration, language)
     end
     local input = mattata.input(message.text)
     if not input
-    or not input:match('%w+')
+    or not input:match('.+')
     then
         return mattata.send_reply(
             message,
@@ -40,7 +40,7 @@ function unfilter:on_message(message, configuration, language)
         )
     end
     local words = {}
-    for word in input:gmatch('%w+')
+    for word in input:gmatch('.+')
     do
         table.insert(
             words,
